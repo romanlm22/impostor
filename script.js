07e7a954d015979254e24f7cbef0bf30c1320825
romanlm22 <machadovrl@gmail.com>
2025-12-12 04:07:10 -0300

    commi

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const categoryImages = {
     animales: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Cartoon_elephant_02.svg/120px-Cartoon_elephant_02.svg.png",
     famosos: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/120px-Albert_Einstein_Head.jpg",
     GrupoRoman: null,
-    GrupoMaxi: null
+    GrupoMaxi: null,
+    Grupo: "https://images.pexels.com/photos/10034072/pexels-photo-10034072.jpeg",
 };
 
 // --- DATOS (PALABRAS CON IMÁGENES INDIVIDUALES) ---
@@ -99,6 +100,7 @@ const data = {
     // Los grupos no tienen imágenes por palabra, solo texto.
     GrupoRoman: ["FABRICIO ", "BRUNO", "ARMANDO", "JERE", "DANTE", "LOLO", "JOACO", "MARIO", "Francici", "MAURO", "Juani"],
     GrupoMaxi: ["Maxi", "Agustin", "ExE", "PINI", "GERMAN", "FABRI", "GUSTAVO", "JOEL"],
+    Grupo: [{word: "Roman", img: "https://images.pexels.com/photos/10034072/pexels-photo-10034072.jpeg"}]
 };
 
 // --- ESTADO ---
